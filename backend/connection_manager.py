c6ec29c1ae8ac0764dc162336e30cca034b2a184
Carl Kaziboni <carlkazibonicman@hotmail.com>
2025-10-25 15:37:28 +0100

    Connection Manager

--- backend/connection_manager.py
+++ backend/connection_manager.py
@@ -1,16 +1,19 @@
+from fastapi import WebSocket
+
+
 class ConnectionManager:
     def __init__(self):
-        self.active_connections = []
+        self.active_connections: list[WebSocket] = []
 
-    def connect(self, websocket):
+    def connect(self, websocket: WebSocket):
         self.active_connections.append(websocket)
 
-    def disconnect(self, websocket):
+    def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, message, websocket):
+    async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
-    async def broadcast(self, message):
+    async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
